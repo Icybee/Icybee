2730d2954077f4bccb51db6474aa7b41a2001d71
Olivier Laviale <contact@weirdog.com>
2012-01-11 18:27:24 +0100

    Added the 'scope' and 'terms' magic properties

--- modules/taxonomy.vocabulary/primary.ar.php
+++ modules/taxonomy.vocabulary/primary.ar.php
@@ -23,15 +23,110 @@ class Vocabulary extends ActiveRecord
 	const IS_MULTIPLE = 'is_multiple';
 	const IS_REQUIRED = 'is_required';
 	const WEIGHT = 'weight';
-
 	const SCOPE = 'scope';
 
+	/**
+	 * Identifier of the vocabulary
+	 *
+	 * @var int
+	 */
 	public $vid;
+
+	/**
+	 * Identifier of the site the vocabulary is attached to.
+	 *
+	 * This value maybe 0, indicating that the vocabulary is not attached to any site.
+	 *
+	 * @var int
+	 */
 	public $siteid;
+
+	/**
+	 * Name of the vocabulary.
+	 *
+	 * @var string
+	 */
 	public $vocabulary;
+
+	/**
+	 * Version of the {@vocabulary} property that can be used in URLs. Written in lowercase, it
+	 * contains only unaccentuated letters, numbers and hyphens.
+	 *
+	 * @var string
+	 */
 	public $vocabularyslug;
+
+	/**
+	 * Can terms be defined as coma-separated values ?
+	 *
+	 * @var bool
+	 */
 	public $is_tags;
+
+	/**
+	 * Can multiple terms be associated to a node ?
+	 *
+	 * @var bool
+	 */
 	public $is_multiple;
+
+	/**
+	 * Is the vocabulary required for the associated scope ?
+	 *
+	 * @var bool
+	 */
 	public $is_required;
+
+	/**
+	 * Weight of the vocabulary relative to other vocabulary.
+	 *
+	 * @var int
+	 */
 	public $weight;
+
+	/**
+	 * Removes the `scope` and `terms` properties.
+	 *
+	 * @see ICanBoogie.ActiveRecord::__sleep()
+	 */
+	public function __sleep()
+	{
+		$properties = parent::__sleep();
+
+		$properties = array_flip($properties);
+
+		unset($properties['scope']);
+		unset($properties['terms']);
+
+		return array_flip($properties);
+	}
+
+	/**
+	 * Returns the scope of the vocabulary, that is the constructors to which the vocabulary is
+	 * associated.
+	 *
+	 * @return array[]string
+	 */
+	protected function __get_scope()
+	{
+		global $core;
+
+		return $core->models['taxonomy.vocabulary/scopes']->select('constructor')
+		->find_by_vid($this->vid)->all(\PDO::FETCH_COLUMN);
+	}
+
+	/**
+	 * Returns the terms associated to this vocabulary, ordered by weight.
+	 *
+	 * @return array[]Term
+	 */
+	protected function __get_terms()
+	{
+		global $core;
+
+		$model = $core->models['taxonomy.terms'];
+
+		return $model->select('term.*')->find_by_vid($this->vid)
+		->order('weight')->all(\PDO::FETCH_CLASS, __NAMESPACE__ . '\Term', array($model));
+	}
 }
