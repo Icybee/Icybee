247b06b74d40ea5db7e2687dc0f91ae5ba1b3f30
Olivier Laviale <contact@weirdog.com>
2012-02-19 13:08:13 +0100

    Removed the 'organize.lists' module for now

--- modules/organize.lists/config/core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array
-(
-	'autoload' => array
-	(
-		'WdAdjustNodesListWidget' => $path . 'widgets/adjust-nodes-list.php'
-	)
-);
--- modules/organize.lists/locale/fr.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return array
-(
-	'Lists' => 'Listes'
-);
