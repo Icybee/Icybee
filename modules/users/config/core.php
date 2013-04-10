d6f55a890c2d7d97d92b72c72b380c5f073bb7e4
Olivier Laviale <contact@weirdog.com>
2013-04-10 17:57:58 +0200

    The Users module is now a package

--- modules/users/config/core.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array
-(
-	'config constructors' => array
-	(
-		'user' => array('merge', 'user')
-	)
-);
--- modules/users/views/nonce_login_request.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Icybee\Modules\Users;
-
-$form = new NonceRequestForm();
-$form->add_class('form-inline');
-
-echo $form;
