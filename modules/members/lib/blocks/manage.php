1d917ebdb139f4de7a4ebf94ca83c6ef5a424612
Olivier Laviale <contact@weirdog.com>
2013-07-25 11:50:15 +0200

    Removed deprecated method

--- modules/members/lib/blocks/manage.php
+++ modules/members/lib/blocks/manage.php
@@ -13,15 +13,5 @@ namespace Icybee\Modules\Members;
 
 class ManageBlock extends \Icybee\Modules\Users\ManageBlock
 {
-	protected function render_cell_uid($record, $property)
-	{
-		$rc = '';
 
-		if ($this->photo)
-		{
-			$rc .= '<img src="' . $this->thumbnail('$icon') . '" class="icon" alt="' . $this->username . '" />';
-		}
-
-		return $rc .= parent::render_cell_uid($record, $property);
-	}
 }
