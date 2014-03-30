6e348065c094be74a5db4e3b65dfae3aae6fb331
Olivier Laviale <contact@weirdog.com>
2014-03-30 11:57:11 +0200

    Cleanup

--- config/core.php
+++ config/core.php
@@ -2,16 +2,12 @@
 
 $vars_path = ICanBoogie\REPOSITORY . 'vars' . DIRECTORY_SEPARATOR;
 
-return array
-(
+return [
+
 	'cache assets' => file_exists($vars_path . 'enable_assets_cache'),
 	'cache catalogs' => file_exists($vars_path . 'enable_catalogs_cache'),
 	'cache configs' => file_exists($vars_path . 'enable_configs_cache'),
 	'cache modules' => file_exists($vars_path . 'enable_modules_cache'),
-	'cache views' => file_exists($vars_path . 'enable_views_cache'),
+	'cache views' => file_exists($vars_path . 'enable_views_cache')
 
-	'config constructors' => array
-	(
-		'admin_routes' => array('Icybee\Hooks::synthesize_admin_routes', 'routes')
-	)
-);
+];
