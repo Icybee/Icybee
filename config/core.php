f1d3e0dad478302db45fab03e496324e2c67cb0f
Olivier Laviale <contact@weirdog.com>
2014-10-27 14:13:44 +0100

    icybee/module-cache update

--- config/core.php
+++ config/core.php
@@ -7,9 +7,6 @@ $vars_path = \ICanBoogie\REPOSITORY . 'vars' . DIRECTORY_SEPARATOR;
 return [
 
 	'cache assets' => file_exists($vars_path . 'enable_assets_cache'),
-	'cache catalogs' => file_exists($vars_path . 'enable_catalogs_cache'),
-	'cache configs' => file_exists($vars_path . 'enable_configs_cache'),
-	'cache modules' => file_exists($vars_path . 'enable_modules_cache'),
 	'cache views' => file_exists($vars_path . 'enable_views_cache'),
 
 	'exception_handler' => 'Icybee\Hooks::exception_handler'
